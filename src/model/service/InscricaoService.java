12a1cf700c2db5ffe23fe45ad663c9db806ce9cd
joao_ullmann <joao_ullmann@estudante.sesisenai.org.br>
2026-05-22 15:38:02 -0300

    Finalizando service

--- src/model/service/InscricaoService.java
+++ src/model/service/InscricaoService.java
@@ -35,16 +35,92 @@ public class InscricaoService {
                 oficinaExiste = true;
                 oficinaSalvar = oficina;
             }
+        }
+            if(oficinaSalvar.getVagasDisponiveis() <= 0){
+                return false;
 
         }
         for (Inscricao inscricao : repository.listar().values()) {
             if (inscricao.getId() == id) {
                 return false;
 
+            }
+        }
+        if (!alunoExiste) {
+            return false;
+        }
+        if (!oficinaExiste) {
+            return false;
+        }
+        if (oficinaSalvar.getVagasDisponiveis() <= 0) {
+            return false;
+        }
+        repository.salvar(new Inscricao(id, alunoSalvar, oficinaSalvar, Inscricao.Status.ATIVA));
+        oficinaSalvar.setVagasDisponiveis(oficinaSalvar.getVagasDisponiveis() - 1);
+        return true;
+    }
+
+    public boolean atualizar(int id, int idAluno, int idOficina, OficinaRepository oficinaRepository, AlunoRepository alunoRepository, InscricaoRepository repository) {
+
+        boolean alunoExiste = false;
+        boolean oficinaExiste = false;
+        boolean idExiste = false;
+
+        Aluno alunoAtualizar = null;
+        Oficina oficinaAtualizar = null;
+
+        if (id <= 0) {
+            return false;
+        }
+
+        for (Aluno aluno : alunoRepository.listar().values()) {
+
+            if (aluno.getId() == id) {
+                alunoExiste = true;
+                alunoAtualizar = aluno;
+            }
+        }
+        for (Oficina oficina : oficinaRepository.listar().values()) {
+            if (oficina.getId() == id) {
+                oficinaExiste = true;
+                oficinaAtualizar = oficina;
+            }
+
+        }
+        for (Inscricao inscricao : repository.listar().values()) {
+            if (inscricao.getId() == id) {
+                return false;
 
             }
         }
-        repository.salvar(new Inscricao(id, alunoSalvar, oficinaSalvar, Inscricao.Status.ATIVA));
+        if (!alunoExiste) {
+            return false;
+        }
+        if (!oficinaExiste) {
+            return false;
+        }
+        if (oficinaAtualizar.getVagasDisponiveis() <= 0) {
+            return false;
+        }
+        repository.salvar(new Inscricao(id, alunoAtualizar, oficinaAtualizar, Inscricao.Status.ATIVA));
+        oficinaAtualizar.setVagasDisponiveis(oficinaAtualizar.getVagasDisponiveis() - 1);
+
+        return true;
+    }
+
+    public boolean cancelar(int id, int idAluno, int idOficina, OficinaRepository oficinaRepository, AlunoRepository alunoRepository,InscricaoRepository repository){
+
+        boolean idExiste = false;
+        for(Inscricao inscricao : repository.listar().values()) {
+            if (inscricao.getId() == id) {
+                idExiste = true;
+            }
+
+        }
+        if(!idExiste){
+            return false;
+        }
+        repository.listar().get(id).setStatus(Inscricao.Status.CANCELADA);
         return true;
     }
 }
